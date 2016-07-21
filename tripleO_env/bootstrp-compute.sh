b616c21702a1e8f8e78bd71de399fecefeb166ca
lindis <openstack@lindison.com>
2016-07-21 12:25:09 -0700

    updates

--- tripleO_env/bootstrp-compute.sh
+++ tripleO_env/bootstrp-compute.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 
 # install ansible (http://docs.ansible.com/intro_installation.html)
-apt-get -y install software-properties-common
-apt-add-repository -y ppa:ansible/ansible
-apt-get update
-apt-get -y install ansible
-apt-get -y install tree
+yum update
+yum install -y vim
 
 # copy examples into /home/vagrant (from inside the mgmt node)
 # cp -a /vagrant/examples/* /home/vagrant/.examples
---
+++ ubuntu_host/install_deps.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Install depedencies for Vagrant and lib-virt Vagrant plugin.
+# Built on host Ubuntu 16.04
+
+wget https://releases.hashicorp.com/vagrant/1.8.4/vagrant_1.8.4_x86_64.deb
+sudo dpkg -i vagrant_1.8.4_x86_64.deb
+sud apt-get update
+sudo apt-get install -y libxslt-dev libxml2-dev libvirt-dev zlib1g-dev ruby-dev ruby-libvirt
+vagrant plugin install vagrant-libvirt
