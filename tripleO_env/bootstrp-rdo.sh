f86265ebd754044004462cae96250af57e2ef85c
lindis <openstack@lindison.com>
2016-07-21 16:57:48 -0700

    updates

--- tripleO_env/bootstrp-rdo.sh
+++ tripleO_env/bootstrp-rdo.sh
@@ -3,6 +3,18 @@
 # install ansible (http://docs.ansible.com/intro_installation.html)
 yum update
 yum install -y vim tree
+systemctl disable firewalld
+systemctl stop firewalld
+systemctl disable NetworkManager
+systemctl stop NetworkManager
+systemctl enable network
+systemctl start network
+yum update
+yum install -y centos-release-openstack-mitaka
+yum update -y
+yum install -y openstack-packstack
+packstack --allinone
+
 
 # copy examples into /home/vagrant (from inside the mgmt node)
 # cp -a /vagrant/examples/* /home/vagrant/.examples
