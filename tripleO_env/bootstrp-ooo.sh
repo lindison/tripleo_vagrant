16de52eac2e4e1c3ee0d07e9647d3f78e03a3c83
lindis <openstack@lindison.com>
2016-07-21 14:02:37 -0700

    vagrant file

--- tripleO_env/bootstrp-ooo.sh
+++ tripleO_env/bootstrp-ooo.sh
@@ -2,25 +2,16 @@
 
 # install ansible (http://docs.ansible.com/intro_installation.html)
 yum update
-yum install -y vim
+yum install -y vim tree
+#yum install -y yum-utils
+#yum-config-manager --enable rhelosp-rhel-7-server-opt
+#yum -y install epel-release
+# curl -L -o /etc/yum.repos.d/delorean.repo http://buildlogs.centos.org/centos/7/cloud/x86_64/rdo-trunk-master-tripleo/delorean.repo
+# curl -L -o /etc/yum.repos.d/delorean-liberty.repo https://trunk.rdoproject.org/centos7-liberty/current/delorean.repo
+# yum -y install yum-plugin-priorities
+# yum install -y python-tripleoclient
 
 # copy examples into /home/vagrant (from inside the mgmt node)
 # cp -a /vagrant/examples/* /home/vagrant/.examples
 # cp -a /vagrant/m* /home/vagrant
 # chown -R vagrant:vagrant /home/vagrant
-
-# configure hosts file for our internal network defined by Vagrantfile
-cat >> /etc/hosts <<EOL
-
-# ansible lab nodes
-192.168.33.10  acs
-192.168.33.20  lb1
-192.168.33.31  dev1
-192.168.33.32  dev2
-192.168.33.21  web1
-192.168.33.22  web2
-192.168.33.23  web3
-192.168.33.24  web4
-192.168.33.25  web5
-192.168.33.50  db1
-EOL
