49613d06cfdfc4bd7c47eae1716a9cc1da0730a1
lindison <openstack@lindison.com>
2016-07-21 14:51:40 -0700

    updates

---
+++ ubuntu_host/README.md
@@ -0,0 +1,2 @@
+# This is to configure Vagrant to use libvirt on Ubuntu
+- This will install Vagrant 1.8.5, dependencies for vagrant plugin libvirt
--- ubuntu_host/install_deps.sh
+++ ubuntu_host/install_deps.sh
@@ -2,8 +2,8 @@
 # Install depedencies for Vagrant and lib-virt Vagrant plugin.
 # Built on host Ubuntu 16.04
 
-wget https://releases.hashicorp.com/vagrant/1.8.4/vagrant_1.8.4_x86_64.deb
-sudo dpkg -i vagrant_1.8.4_x86_64.deb
+wget https://releases.hashicorp.com/vagrant/1.8.5/vagrant_1.8.5_x86_64.deb
+sudo dpkg -i vagrant_1.8.5_x86_64.deb
 sud apt-get update
 sudo apt-get install -y libxslt-dev libxml2-dev libvirt-dev zlib1g-dev ruby-dev ruby-libvirt
 vagrant plugin install vagrant-libvirt
