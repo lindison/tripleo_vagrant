4b9f30b99e042be59cc297cb314ac9b27077cbfb
lindis <openstack@lindison.com>
2016-07-22 18:11:01 -0700

    updates

---
+++ tripleO_env/bootstrp-ceph.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# install ansible (http://docs.ansible.com/intro_installation.html)
+yum update
+yum install -y vim tree
+# copy examples into /home/vagrant (from inside the mgmt node)
+# cp -a /vagrant/examples/* /home/vagrant/.examples
+# cp -a /vagrant/m* /home/vagrant
+# chown -R vagrant:vagrant /home/vagrant
