1c1c4ca5fc890afb8eee8c02fb8c742a0c6831e8
lindis <openstack@lindison.com>
2016-07-22 17:16:30 -0700

    updates

--- tripleO_env/bootstrp-ansible.sh
+++ tripleO_env/bootstrp-ansible.sh
@@ -13,12 +13,34 @@ cp -a /vagrant/playbooks/* /home/vagrant/playbooks
 # cp -a /vagrant/m* /home/vagrant
 chown -R vagrant:vagrant /home/vagrant
 
-# Configure Ansible Hosts file.
+cp /vagrant/id_rsa /home/vagrant/.ssh/
+cp /vagrant/id_rsa.pub /home/vagrant/.ssh/
+chown vagrant:vagrant /home/vagrant/.ssh/id_rsa
+chown vagrant:vagrant /home/vagrant/.ssh/id_rsa.pub
 
+# Create names file for ssh-keyscan and ssh-copy-id
+# Put server names in here for use with Ansible
+cp /vagrant/names /home/vagrant/names
+chown vagrant:vagrant /home/vagrant/names
+touch /home/vagrant/names
+
+# Create pass for sshpass
+cat >> /home/vagrant/pass.txt <<EOL
+vagrant
+EOL
+chown vagrant:vagrant /home/vagrant/pass.txt
+runuser -l vagrant -c "/home/vagrant/ssh_setup.sh"
+# runuser -l vagrant -c "ssh-keyscan $(cat /home/vagrant/names) >> /home/vagrant/.ssh/known_hosts"
+# runuser -l vagrant -c "for i in $(cat /home/vagrant/names); do sshpass -f pass.txt ssh-copy-id vagrant@$(cat /home/vagrant/names); done"
+# runuser -l vagrant -c "for i in $(cat /home/vagrant/names); do sshpass -f pass.txt ssh-copy-id vagrant@$(cat /home/vagrant/names); done"
+
+# Configure Ansible Hosts file, update with hosts names for each component.
 cat >> /etc/ansible/hosts <<EOL
 
 [undercloud]
 ooo01
+ooo02
+ooo03
 
 [overcloud]
 rdo01
