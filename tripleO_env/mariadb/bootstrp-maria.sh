052c751aa6328963d12f6213f1ab00edfc146e38
lindison <openstack@lindison.com>
2016-07-26 16:13:13 -0700

    updates

---
+++ tripleO_env/mariadb/bootstrp-maria.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# install ansible (http://docs.ansible.com/intro_installation.html)
+
+# copy examples into /home/vagrant (from inside the mgmt node)
+# cp -a /vagrant/examples/* /home/vagrant/.examples
+# cp -a /vagrant/m* /home/vagrant
+# chown -R vagrant:vagrant /home/vagrant
+
+# configure hosts file for our internal network defined by Vagrantfile
+
