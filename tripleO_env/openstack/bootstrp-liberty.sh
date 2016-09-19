cbaf09390e14bd7f25d29eeca3548f2c0a96b89f
lindison <openstack@lindison.com>
2016-09-19 11:58:32 -0700

    updates

--- tripleO_env/openstack/bootstrp-liberty.sh
+++ tripleO_env/openstack/bootstrp-liberty.sh
@@ -12,7 +12,7 @@ yum update
 yum install -y centos-release-openstack-liberty
 yum update -y
 yum install -y openstack-packstack
-packstack --allinone --answer-file=/home/vagrant/sync/packstack-answers.txt
+packstack --allinone
 cp /root/keystonerc_admin /home/vagrant/keystonerc_admin
 chown vagrant:vagrant /home/vagrant/keystonerc_admin
 
