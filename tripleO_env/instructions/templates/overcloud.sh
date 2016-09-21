5f65b6b2174f629cba46ad11707d1deee4bad1b5
lindison <openstack@lindison.com>
2016-09-20 22:35:25 -0400

    adding tempaltes

---
+++ tripleO_env/instructions/templates/overcloud.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+openstack overcloud deploy --templates --validation-errors-fatal --validation-warnings-fatal \
+          -e /home/stack/templates/userdata_env.yaml \
+          -e /home/stack/templates/lab-network-environment.yaml \
+          -e /home/stack/templates/timezone.yaml \
+          -t 120 \
+          --control-flavor control \
+          --compute-flavor compute \
+          --control-scale 3 \
+          --compute-scale 1 \
+          --ceph-storage-scale 0 \
+          --swift-storage-scale 0 \
+          --block-storage-scale 0 \
+          --ntp-server 24.56.178.140
+
+
+
