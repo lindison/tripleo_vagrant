3bf8fbe05c2c5b0c02edb6d39aa355078aee8451
lindison <openstack@lindison.com>
2016-09-23 20:56:57 -0400

    update

--- tripleO_env/monitoring/rsyslog/ansible_inv.sh
+++ tripleO_env/monitoring/rsyslog/ansible_inv.sh
@@ -6,6 +6,7 @@ nova list | awk '$6 == "ACTIVE" {print $4,$12};' > /tmp/ansible_nodes
 nova list | awk '$6 == "ACTIVE" {print $12};' > /tmp/ips_nodes
 nova list | awk '$6 == "ACTIVE" {print $4};' > /tmp/names_nodes
 awk '{gsub("ctlplane=", "");print}' /tmp/ips_nodes > /tmp/ips
+cat /home/stack/hosts.orig > /etc/hosts
 cat /tmp/hostnames >> /etc/hosts
 rm /home/stack/.ssh/known_hosts
 for i in $(cat /tmp/ips); do ssh-keyscan $i >> /home/stack/.ssh/known_hosts; done
