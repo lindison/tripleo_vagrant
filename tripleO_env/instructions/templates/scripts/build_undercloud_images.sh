a5e0e1b09e157eec004ad3997b90319e71ea7fb1
lindison <openstack@lindison.com>
2016-09-28 12:37:26 -0400

    updates

---
+++ tripleO_env/instructions/templates/scripts/build_undercloud_images.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo yum -y install rhosp-director-images rhosp-director-images-ipa
+cp /usr/share/rhosp-director-images/overcloud-full-latest-8.0.tar ~/images/.
+cp /usr/share/rhosp-director-images/ironic-python-agent-latest-8.0.tar ~/images/.
+cd ~/images
+for tarfile in *.tar; do tar -xf $tarfile; done
+openstack overcloud image upload --image-path /home/stack/images
